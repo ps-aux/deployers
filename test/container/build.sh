a514714f63e9dbc57113e48e71f0ac0209c5d39d
psaux <seriouz.accz@gmail.com>
2019-12-26 15:13:57 +0100

    [ci] try fix SSH tests

---
+++ test/container/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+name="eu.gcr.io/devops-247114/deployers-test-container"
+tag=$1
+
+if [[ -z ${tag} ]];then
+    echo "Tag not provided"
+    exit 1
+fi
+
+image_name=${name}:${tag}
+docker build . -t  ${image_name}
+
+docker push  ${image_name}
