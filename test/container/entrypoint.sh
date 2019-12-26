1f4dd9dbef967f46d8f66b8f05133ebff3742c0b
psaux <seriouz.accz@gmail.com>
2019-12-26 14:02:35 +0100

    [test container] Add support for authorized ssh key via env var

---
+++ test/container/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+ssh_key=${SSH_AUTHORIZED_KEY}
+echo "Running test container. SSH_AUTHORIZED_KEY=${ssh_key}"
+
+
+if [[ ! -z ${ssh_key} ]];then
+    ssh_dir="/home/ops/.ssh"
+    mkdir -p ${ssh_dir}
+    echo "Using provided SSH key '${ssh_key}' as authorized key"
+
+    tmp_file=/tmp/ssh-key
+
+    echo ${ssh_key} > ${tmp_file}
+
+    # Check the key
+    ssh-keygen -l -f ${tmp_file}
+
+    mv ${tmp_file}  ${ssh_dir}/authorized_keys
+fi
+/usr/sbin/sshd -D
