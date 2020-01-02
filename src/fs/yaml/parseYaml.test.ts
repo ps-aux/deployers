43fd568889bfd32895e7d34b0adcb09a5d517a5a
psaux <seriouz.accz@gmail.com>
2020-01-02 10:11:12 +0100

    Fix parseYaml.test.ts

--- src/fs/yaml/parseYaml.test.ts
+++ src/fs/yaml/parseYaml.test.ts
@@ -8,5 +8,17 @@ it('works', () => {
 
     const res = parseYaml(content)
 
-    console.log(res)
+    expect(res).toMatchObject({
+        version: '3',
+        services: {
+            app: {
+                image: 'gcr.io/kuar-demo/kuard-amd64:{{ version }}',
+                ports: ['8081:8080', '8090:8080'],
+                environment: {
+                    // eslint-disable-next-line no-template-curly-in-string
+                    FOO: '${FOO}'
+                }
+            }
+        }
+    })
 })
